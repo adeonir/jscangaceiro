fb86969b9cb90f3350f93fe0494b584c6551cb0d
Adeonir Kohl <adeonir@gmail.com>
2019-02-12 15:01:15 -0200

    Refactor string to date

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,15 +9,10 @@ class NegociacaoController {
     event.preventDefault()
 
     let data = new Date(
-      ...this._inputData.value.split('-').map((item, index) => {
-        if (index == 1) {
-          return item - 1
-        }
-        return item
-      })
+      ...this._inputData.value
+        .split('-')
+        .map((item, index) => item - (index % 2))
     )
-
-    console.log(data)
 
     let negociacao = new Negociacao(
       data,
