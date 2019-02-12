d8289662f48dbb9e930e975c55e0f0003daf6cc6
Adeonir Kohl <adeonir@gmail.com>
2019-02-12 00:26:53 -0200

    Fix typo

--- client/app/index.js
+++ client/app/index.js
@@ -1,7 +1,7 @@
 var campos = [
   document.querySelector('#data'),
-  document.querySelector('#quantidade')
-  document.querySelector('#valor'),
+  document.querySelector('#quantidade'),
+  document.querySelector('#valor')
 ]
 
 var tbody = document.querySelector('table tbody')
@@ -16,14 +16,14 @@ document.querySelector('.form').addEventListener('submit', function(event) {
     tr.appendChild(td)
   })
 
-    var tdVolume = document.createElement('td')
-    tdVolume.textContent = campos[1].value * campos[2].value
-    tr.appendChild(tdVolume)
+  var tdVolume = document.createElement('td')
+  tdVolume.textContent = campos[1].value * campos[2].value
+  tr.appendChild(tdVolume)
 
-    tbody.appendChild(tr)
+  tbody.appendChild(tr)
 
-    campos[0].value = ''
-    campos[1].value = 1
-    campos[2].value = 0
-    campos[0].focus()
+  campos[0].value = ''
+  campos[1].value = 1
+  campos[2].value = 0
+  campos[0].focus()
 })
